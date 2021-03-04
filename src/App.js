4c196cb62d4cc2fb3834543673036ffd862212c9
Dezang <dezang@dezang.net>
2021-03-05 01:11:29 +0900

    feat: Handling Relationships

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-// import { Admin, Resource, ListGuesser } from 'react-admin'
-import { Admin, Resource } from 'react-admin'
+import { Admin, Resource, ListGuesser } from 'react-admin'
+// import { Admin, Resource } from 'react-admin'
 import { UserList } from './users'
+import { PostList } from './posts'
 import jsonServerProvider from 'ra-data-json-server'
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com')
@@ -8,6 +9,7 @@ const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com')
 function App() {
   return (
     <Admin dataProvider={dataProvider}>
+      <Resource name="posts" list={PostList} />
       <Resource name="users" list={UserList} />
     </Admin>
   );
---
+++ src/posts.js
@@ -0,0 +1,14 @@
+import { List, Datagrid, TextField, ReferenceField, EditButton } from 'react-admin'
+
+export const PostList = props => (
+    <List {...props}>
+        <Datagrid rowClick="edit">
+            <TextField source="id" />
+            <ReferenceField source="userId" reference="users">
+                <TextField source="name" />
+            </ReferenceField>
+            <TextField source="title" />
+            <EditButton />
+        </Datagrid>
+    </List>
+);
