3be274bfd311c728651b7cabb15792fa73c41ecd
Dezang <dezang@dezang.net>
2021-03-05 00:55:00 +0900

    feat: customizing styles

--- src/MyUserField.js
+++ src/MyUserField.js
@@ -1,7 +1,22 @@
+import { makeStyles } from '@material-ui/core/styles';
+import LaunchIcon from '@material-ui/icons/Launch';
+
+const useStyles = makeStyles({
+    link: {
+        textDecoration: 'none',
+    },
+    icon: {
+        width: '0.5em',
+        paddingLeft: 2,
+    }
+})
+
 function MyUserField({ record = {}, source}) {
+    const classes = useStyles()
     return (
-        <a href={record[source]}>
+        <a href={record[source]} className={classes.link}>
             {record[source]}
+            <LaunchIcon className={classes.icon}/>
         </a>
     )
 }
